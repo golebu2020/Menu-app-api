0e3772d23f11222a4203c5aa83c5b8af2356fa27
Benedict Olebu <cgolebu.dev@gmail.com>
2023-07-07 14:11:03 -0700

    modified calc.py & test.py

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Add x and y and return results."""
+    return x + y
+
+def subtract(x,y):
+    """Subtact x from y and return result"""
+    return y - x
+
+
+
+
